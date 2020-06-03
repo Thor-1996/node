83e2619232e2f4eb961c30fa85d65fc8ddf553db
Thor <15073245605@163.com>
2020-06-03 18:30:43 +0800

    完成阿里云文件上传功能

--- aliyunUpload/node.js
+++ aliyunUpload/node.js
@@ -1,5 +1,5 @@
-var http = require("http");
-var queryString = require("querystring");
+var http = require("http"),
+  formidable = require("formidable");
 const arr = [
   76,
   84,
@@ -34,15 +34,6 @@ const client = new OSS({
   bucket: "oss-zhao",
 });
 
-async function upload(fileName) {
-  try {
-    await client.put(fileName, new Buffer("hello world"));
-    console.log("上传成功");
-  } catch (e) {
-    console.log(e, "上传错误的原因");
-  }
-}
-
 const server = http.createServer();
 
 server.on("request", function (req, res) {
@@ -55,17 +46,25 @@ server.on("request", function (req, res) {
     );
 
     if (req.url.indexOf("uploadfile") !== -1) {
-      upload("text1.txt");
-      let obj = null;
-      let currentData = "";
-      req.on("data", function (data) {
-        console.log(data, "传来的数据");
-        currentData += data;
-        obj = queryString.parse(currentData);
-        console.log(obj, "格式化后的数据");
-      });
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+        if (err) {
+          console.error(err.message);
+          res.end(err);
+          return;
+        }
 
-      res.end("success");
+        client
+          .put("image/" + fields.fileName, files.file.path)
+          .then((r) => {
+            console.log(r);
+            res.end(r.url);
+          })
+          .catch((e) => {
+            console.log(e, "失败");
+            res.end("failure");
+          });
+      });
     }
   } catch (err) {
     res.end(err);
@@ -73,56 +72,4 @@ server.on("request", function (req, res) {
 });
 
 server.listen("8083");
-//用于提示我们服务器启动成功
 console.log("Server running at 8083 port!");
-
-// var fs=require("fs");
-
-// http.createServer(function(req,res){
-// 	res.writeHead(200,{"Content-type":"text/html;charset=UTF-8","Access-Control-Allow-Origin":"*"});
-// 	if (req.method.toLowerCase()=="post") {
-// 		//新建一个空数组接受流的信息
-// 		var chunks=[];
-// 		//获取长度
-// 		let num=0;
-
-// 		req.on("data",function(chunk){
-// 			chunks.push(chunk);
-// 			num+=chunk.length;
-// 		});
-// 		req.on("end",function(){
-// 			//最终流的内容本体
-// 			var buffer = Buffer.concat(chunks, num);
-// 			//新建数组接收出去\r\n的数据下标
-// 			let rems=[];
-// 			//根据\r\n分离数据和报头
-// 			for (var i = 0; i < buffer.length; i++) {
-// 				let v=buffer[i];
-// 				let v2=buffer[i+1];
-// 			// 10代表\n 13代表\r
-// 			if (v==13&&v2==10) {
-// 				rems.push(i)
-// 			}
-// 		}
-// 		//获取上传图片信息
-// 		let picmsg_1 = buffer.slice(rems[0]+2,rems[1]).toString();
-// 		console.log(picmsg_1);
-// 		let filename = picmsg_1.match(/filename=".*"/g)[0].split('"')[1];
-// 		console.log(filename);
-
-// 		 //图片数据
-// 		 var nbuf = buffer.slice(rems[3]+2,rems[rems.length-2]);
-// 		 let address="./"+filename;
-// 		 //创建空文件并写入内容
-// 		 fs.writeFile(address,nbuf,function(err){
-// 		 	if (err) {
-// 		 		console.log(err);
-// 		 	}else{
-// 		 		console.log("创建成功")
-// 		 	}
-// 		 })
-
-// 		})
-// 		res.end();
-// 	}
-// }).listen(3000,"localhost")
